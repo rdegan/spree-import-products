6982cd535b32884fb52aab003fd1814049b5ec13
Aleksey Demidov <aleksey.dem@gmail.com>
2012-02-02 13:45:58 +0400

    Namespaces for controller

--- app/controllers/spree/admin/product_imports_controller.rb
+++ app/controllers/spree/admin/product_imports_controller.rb
@@ -1,22 +1,21 @@
-class Admin::ProductImportsController < Admin::BaseController
-
+class Spree::Admin::ProductImportsController < Spree::Admin::BaseController
   #Sorry for not using resource_controller railsdog - I wanted to, but then... I did it this way.
   #Verbosity is nice?
   #Feel free to refactor and submit a pull request.
-
+  
   def index
-    redirect_to :action => :new
+    redirect_to :new
   end
-
+  
   def new
     @product_import = ProductImport.new
   end
-
-
+  
+  
   def create
     @product_import = ProductImport.create(params[:product_import])
     Delayed::Job.enqueue ImportProducts::ImportJob.new(@product_import, @current_user)
     flash[:notice] = t('product_import_processing')
-    redirect_to admin_product_imports_path
+    redirect_to :new
   end
 end
