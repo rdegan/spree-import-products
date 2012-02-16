86f53deb4ab172eb5a3e70f89307a127cd704408
Sergey Korotkov <sergey.korotkov@me.com>
2012-02-16 19:58:09 +0800

    deface

--- lib/import_products_hooks.rb
+++
@@ -1,8 +0,0 @@
-class ImportProductsHooks < Spree::ThemeSupport::HookListener
-  # custom hooks go here
-  Deface::Override.new(:virtual_path => "spree/layouts/admin",
-                       :name => "product_imports_admin_tab",
-                       :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                       :text => "<%=tab(:product_imports,  :route => 'admin_product_imports' )%>",
-                       :disabled => false)
-end
