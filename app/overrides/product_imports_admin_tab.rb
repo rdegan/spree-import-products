82b139cc03883479d504315a9fa967d63eaa7021
Sergey Korotkov <sergey.korotkov@me.com>
2012-02-16 15:58:09 +0400

    deface

---
+++ app/overrides/product_imports_admin_tab.rb
@@ -0,0 +1,5 @@
+Deface::Override.new(:virtual_path => "spree/layouts/admin",
+                     :name => "product_imports_admin_tab",
+                     :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
+                     :text => "<%=tab(:product_imports,  :route => 'admin_product_imports' )%>",
+                     :disabled => false)
